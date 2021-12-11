906c4b061ff44bce2d1b652c06749e7878b52a24
Nikita(PC) <nikita.verba04@mail.ru>
2021-12-11 17:10:22 +0200

    Added models Driver and Vehicle

--- main/models.py
+++ main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.validators import RegexValidator
+plate_number_validator = RegexValidator(r"[А-Я]{2}\d{4}[А-Я]{2}", "Input number in format 'AЯ1234AБ'")
 
-# Create your models here.
+
+class Driver(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Vehicle(models.Model):
+    driver_id = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    plate_number = models.CharField(max_length=15, validators=[plate_number_validator])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.make} {self.model}"
